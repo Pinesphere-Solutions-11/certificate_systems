3b18820694579bd3d3eee07065b26c8787773667
Praveen Kumar <praveenmurugan420@gmail.com>
2025-07-24 12:20:41 +0530

    admin comment

--- certificate_system/settings.py
+++ certificate_system/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'certificate_system',
         'USER' : 'postgres',
-        'PASSWORD' : '123',
+        'PASSWORD' : 'postgres',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
